03222b9c3ba419f618b9b6ab05c2df853450de2e
Rafi Hariman <rafi.hariman@uii.ac.id>
2024-03-05 10:15:18 +0700

    add pie chart for test

--- app-js/bottom-nav-bar.js
+++ app-js/bottom-nav-bar.js
@@ -1,6 +1,4 @@
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-
 function scrollActive(){
     const scrollY = window.pageYOffset
 
@@ -19,12 +17,25 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
-
+new Chart(document.getElementById('pie-chart'), {
+    type: 'pie',
+    data: {
+      labels: ["Motorik Halus", "Motorik Kasar", "Sosialisasi dan Kemandirian", "Bicara dan Bahasa"],
+      datasets: [{
+        backgroundColor: ["#e63946", "#254BDD", "#ffbe0b", "#1d3557"],
+        data: [15, 20, 30, 3]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Tumbuh Kembang Bayi Pie Chart'
+      },
+      responsive: true
+    }
+});
